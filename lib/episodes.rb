2008912480901c4771b29fb4472cd3fe6e215cf7
Your Name <racmalin@yahoo.com>
2020-07-18 19:41:00 -0500

    Save all new

---
+++ lib/episodes.rb
@@ -0,0 +1,8 @@
+module SopranosCli
+
+    class EPISODES
+    
+    
+    end
+
+end
--- lib/sopranos_cli.rb
+++ lib/sopranos_cli.rb
@@ -1,5 +1,7 @@
 require "sopranos_cli/version"
-
+require "sopranos_cli/api"
+require "sopranos_cli/cli"
+require "sopranos_cli/episodes"
 module SopranosCli
   class Error < StandardError; end
   # Your code goes here...
