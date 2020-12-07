a46b03c4c36ec1db38b308003fd58a12c6b83db4
racmalin <racmalin@yahoo.com>
2020-12-07 14:46:22 -0700

    initial commit

--- lib/sopranos_cli/cli.rb
+++ lib/sopranos_cli/cli.rb
@@ -5,7 +5,7 @@ module SopranosCli
         puts "Hey!Forget about it! Welcome to the Sopranos CLI app."
         puts "There are six seasons of The Sopranos,
           Choose 1 thru 6 to view all the episodes within that season,then choose an episode to see a brief summary,
-          or exit twice to quit."
+          or exit to quit."
           if @season_input != "exit" && @season_input != "back"
             @season_input = gets.chomp
             if @season_input.to_i <= 6 && @season_input.to_i > 0
@@ -31,10 +31,10 @@ module SopranosCli
             end
             
             if @input.to_i != "exit"
-             puts "Would you like to exit, or search again?"
+             puts "Would you like to exit, or enter to search again?"
             end
             while @input == gets.chomp
-             return start
+              start
             end
           
            unless @input.to_i == "exit"
